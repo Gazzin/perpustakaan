d722b80f5fc384829475a1316d274d5e07f400f0
maulanazinedin <maulanazinedinzidane@gmail.com>
2018-07-18 22:32:12 +0700

    Menambahkan Pengembalian
    Menambahkan pengembalian dan merubah laporan

--- application/views/admin/transaksi/laporan.php
+++ application/views/admin/transaksi/laporan.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h1>Laporan</h1>
-			<table class="table table-striped table-bordered">
+			<table id="table-laporan" class="table table-striped table-bordered">
 				<thead>
 					<tr>
 						<th>No_Pinjam</th>
@@ -12,56 +12,49 @@
 						<th>Nama Petugas</th>
 						<th>Nama Pengguna</th>
 						<th>Status</th>
-						<th>Judul Buku</th>
-						<th>Jumlah</th>
-						<th>Harga Buku</th>
+						<th>Buku</th>
 						<th>Total</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php $total=0; ?>
 					<?php foreach ($peminjaman as $key => $value): ?>
-						<?php $total += $value->total ?>
 						<?php 
+						$total += $value->total;
 						$buku = explode("<br>",$value->list);
 						$rowspan = count($buku);
 						?>
 						<tr>
-							<td rowspan="<?php echo $rowspan ?>"><?php echo $value->no_pinjam ?></td>
-							<td rowspan="<?php echo $rowspan ?>"><?php echo $value->tanggal ?></td>
-							<td rowspan="<?php echo $rowspan ?>"><?php echo $value->nama_petugas ?></td>
-							<td rowspan="<?php echo $rowspan ?>"><?php echo $value->nama_pengguna ?></td>
-							<td rowspan="<?php echo $rowspan ?>">
-								<?php if($value->status == 1): ?>
+							<td><?php echo $value->no_pinjam ?></td>
+							<td><?php echo $value->tanggal ?></td>
+							<td><?php echo $value->nama_petugas ?></td>
+							<td><?php echo $value->nama_pengguna ?></td>
+							<td>
+								<?php if($value->status == 1){ ?>
 									Belum Dikembalikan
-								<?php endif ?>
+								<?php }else{ ?>
+									Sudah Dikembalikan
+								<?php } ?>
 							</td>
-							<?php $buk = explode("-",$buku[0]) ?>
-							<td><?php echo $buk[0] ?></td>
-							<td><?php echo $buk[1] ?></td>
-							<td><?php echo $buk[2] ?></td>
-							<td rowspan="<?php echo $rowspan ?>"><?php echo $value->total ?></td>
+							<td>
+								<?php $buku_list = explode("<br>", $value->list); ?>
+								<?php foreach ($buku_list as $k => $v): ?>
+									<?php $buku = explode("-", $v); ?> 
+									<p><b><?php echo $buku[0] ?></b> @<?php echo $buku[1] ?>, Rp. <?php echo $buku[2] ?></p>
+								<?php endforeach ?>
+							</td>
+							<td><?php echo $value->total ?></td>
 						</tr>
-						<?php unset($buku[0]); ?>
-						<?php foreach ($buku as $k => $v): ?>
-							<?php $bu = explode("-",$v) ?>
-							<tr>
-								<td><?php echo $bu[0] ?></td>
-								<td><?php echo $bu[1] ?></td>
-								<td><?php echo $bu[2] ?></td>
-							</tr>
-						<?php endforeach ?>
 					<?php endforeach ?>
 				</tbody>
 				<tfoot>
 					<tr>
-						<td colspan="7"></td>
+						<td colspan="5"></td>
 						<td>Total</td>
 						<td><?php echo $total ?></td>
 					</tr>
 				</tfoot>
 			</table>
 		</div>
-
 	</main>
 	<?php $this->load->view('admin/footer') ?>
